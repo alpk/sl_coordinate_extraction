2b9cccccd3f54457b91e1af7c6534ee71ce47439
aalpk <alpkindiroglu@gmail.com>
2022-03-22 08:24:52 +0000

    modify script

--- scripts/run_mp_csl.sh
+++ scripts/run_mp_csl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --partition=long
+#SBATCH --partition=longer
 #SBATCH -N 1
 #SBATCH -n 10
 # #SBATCH --mem=64000M
-#SBATCH -J mediapipe
+#SBATCH -J cpu_fe_mp
 #SBATCH --output=slurm_out/test-%j.out
 #SBATCH --error=slurm_out/test-%j.err
 
 
-/cta/users/akindiroglu/workspace/libs/torch_env/bin/python main.py --base_path /cta/dark/CSLR/frames/ --save_path /cta/dark/CSLR/skeleton_mediapipe  --number_of_cores 10
+/cta/users/akindiroglu/workspace/libs/torch_env/bin/python main.py --base_path /scratch/users/akindiroglu/CSL/frames/ --save_path /scratch/users/akindiroglu/CSL/skeleton_mediapipe  --number_of_cores 10
 
