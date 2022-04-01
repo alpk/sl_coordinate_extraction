249a297b395af9f44ce9fa0b678fb675b6825947
Ahmet Alp KINDIROGLU <alpkindiroglu@gmail.com>
2022-04-01 13:22:36 +0300

    time limit

--- scripts/truba/run_pl_CSL.sh
+++ scripts/truba/run_pl_CSL.sh
@@ -2,29 +2,24 @@
 
 #SBATCH --partition=mid2
 #SBATCH --time=08-00:00:00
-#SBATCH --ntasks=4
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 # #SBATCH --mem-per-cpu=10G
 #SBATCH -J cpu_fe_mp
 #SBATCH --output=slurm_out/test-%j.out
 #SBATCH --error=slurm_out/test-%j.err
 
-srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py \
-            --base_path /truba_scratch/akindiroglu/data/CSL/frames/ \
-            --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ \
-            --number_of_cores 1  &
-srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py \
-            --base_path /truba_scratch/akindiroglu/data/CSL/frames/ \
-            --save_path /truba_scratch/akindiroglu/data/CSL//skeleton_mediapipe/ \
-            --number_of_cores 1  &
-srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py \
-            --base_path /truba_scratch/akindiroglu/data/CSL/frames/ \
-            --save_path /truba_scratch/akindiroglu/data/CSL//skeleton_mediapipe/ \
-            --number_of_cores 1  &
-srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py \
-            --base_path /truba_scratch/akindiroglu/data/CSL/frames/ \
-            --save_path /truba_scratch/akindiroglu/data/CSL//skeleton_mediapipe/ \
-            --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+srun -N1 -n1 /truba/home/akindiroglu/Workspace/Libs/pytorch_nightly/bin/python main.py --base_path /truba_scratch/akindiroglu/data/CSL/frames/ --save_path /truba_scratch/akindiroglu/data/CSL/skeleton_mediapipe/ --number_of_cores 1  &
+
 
 wait
 
